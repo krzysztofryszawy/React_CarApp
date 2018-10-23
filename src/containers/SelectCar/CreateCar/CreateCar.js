55ab43a7ddc60ee3645d53ccbbf71ea328f28817
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2018-10-23 17:55:24 +0200

    uploading to database car settings

--- src/containers/SelectCar/CreateCar/CreateCar.js
+++ src/containers/SelectCar/CreateCar/CreateCar.js
@@ -22,23 +22,23 @@ class createCar extends Component {
 
          const saveCarHandler = () => {
              
-                const databaseCarName = `/${this.state.regNumber}`
+                const databaseCarName = `/${this.state.regNumber}.json`
 
                 const newCarData = {
-                    databaseCarName: databaseCarName,
-                    vin: this.state.vin,
-                    productionYear: this.state.productionYear,
-                    name: this.state.name,
-                    regNumber: this.state.regNumber,
-                    timingbeltChangeDate: this.state.timingbeltChangeDate,
-                    mileage: this.state.mileage,
-                    lastInspectionDate: this.state.lastInspectionDate,
-                    lastOilChangeDate: this.state.lastOilChangeDate,
-                    lastOilChangeMileage: this.state.lastOilChangeMileage,
-                    oilType: this.state.oilType,
-                    lastInsuranceDate: this.state.lastInsuranceDate,
-                    lastAirconditionServiceDate: this.state.lastAirconditionServiceDate,
-                    tires: this.state.tires,
+                    databaseCarName: databaseCarName, //jest
+                    vin: this.state.vin,//jest
+                    productionYear: this.state.productionYear,//jest
+                    name: this.state.name,//jest
+                    regNumber: this.state.regNumber,//jest
+                    timingbeltChangeDate: this.state.timingbeltChangeDate,//jest
+                    mileage: this.state.mileage,//jest
+                    lastInspectionDate: this.state.lastInspectionDate,//jest
+                    lastOilChangeDate: this.state.lastOilChangeDate,//jest
+                    lastOilChangeMileage: this.state.lastOilChangeMileage,//jest
+                    oilType: this.state.oilType,//jest
+                    lastInsuranceDate: this.state.lastInsuranceDate,//jest
+                    lastAirconditionServiceDate: this.state.lastAirconditionServiceDate,//jest
+                    tires: this.state.tires,//jest
                 }
 
                 
@@ -66,18 +66,20 @@ class createCar extends Component {
                 }
 
 
-//             
-//
-////                
-//             
-//             
-//
-//                axios.put(databaseCarName, newCarData)
-//                           .then(response => {
-//                            })
-//                            .catch(error => {
-//                                console.log(error)
-//                            });
+                        console.log(databaseCarName)
+             
+
+//                
+             
+
+
+                axios.put(databaseCarName, newCarData)
+                           .then(response => {
+                                console.log(response)
+                            })
+                            .catch(error => {
+                                console.log(error)
+                            });
             }        
 
 
@@ -88,20 +90,57 @@ class createCar extends Component {
             let content = ( <div className={classes.createCarclass}>
                                 <div>
                                     <label>numer rejestracji</label>
-                                    <input  type="text" name="regNumber"  onChange={changeState}/>
+                                    <input name="regNumber"  type="text"  onChange={changeState}/>
                                 </div>
                                 <div>
                                     <label>marka</label>
-                                    <input  type="text" name="name"  onChange={changeState}/>
+                                    <input name="name"  type="text"  onChange={changeState}/>
                                 </div>
                                 <div>
                                     <label>numer VIN</label>
-                                    <input  type="text" name="vin" onChange={changeState}/>
+                                    <input name="vin"  type="text" onChange={changeState}/>
+                                </div>
+                                <div>
+                                    <label>rok produkcji</label>
+                                    <input name="productionYear"  type="text" onChange={changeState}/>
+                                </div>
+                                <div>
+                                    <label>przebieg przy ostatniej wymianie oleju</label>
+                                    <input name="lastOilChangeMileage"  type="text" onChange={changeState}/>
                                 </div>
                                 <div>
                                     <label>aktualny przebieg pojazdu</label>
-                                    <input  type="number" step="1" name="mileage" onChange={changeState}/>
+                                    <input   name="mileage" type="number" step="1" onChange={changeState}/>
                                 </div>
+                                <div>
+                                    <label>zastosowany olej</label>
+                                    <input name="oilType"  type="text" onChange={changeState}/>
+                                </div>
+                                <div>
+                                    <label>typ aktualnie używanych opon (zimowe/letnie)</label>
+                                    <input name="tires"  type="text" onChange={changeState}/>
+                                </div>
+                                <div>
+                                    <label>Wprowadź datę ostatniego przeglądu</label>
+                                    <input name="lastInspectionDate" type="date" onChange={changeState}/>
+                                </div>
+                                <div>
+                                    <label>Wprowadź datę ostatniej wymiany oleju</label>
+                                    <input name="lastOilChangeDate" type="date" onChange={changeState}/>
+                                </div>                                
+                                <div>
+                                    <label>Wprowadź datę ostatniej wymiany rozrządu</label>
+                                    <input name="timingbeltChangeDate" type="date" onChange={changeState}/>
+                                </div>                                
+                                <div>
+                                    <label>Wprowadź datę rozpoczęcia aktualnego ubezpieczenia OC</label>
+                                    <input name="lastInsuranceDate" type="date" onChange={changeState}/>
+                                </div>                                
+                                <div>
+                                    <label>Wprowadź datę ostatniego serwisu klimatyzacji</label>
+                                    <input name="lastAirconditionServiceDate" type="date" onChange={changeState}/>
+                                </div>
+
                                 <div className={classes.confirmation} style={{height: '4em'}}>
                                     <Button btnType='Proceed' clicked={saveCarHandler}>ZAPISZ</Button>
                                     <Button btnType='Cancel' clicked={this.props.createCarSwitcher}>WRÓĆ</Button>
